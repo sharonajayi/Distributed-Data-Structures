2d6866ab011ffe5f147f4f54b7432ab4a68acfb0
sharonajayi <75191908+sharonajayi@users.noreply.github.com>
2021-04-02 20:41:14 -0500

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -2,6 +2,10 @@ package server_sample;
 
 import java.net.ServerSocket;  // The server uses this to bind to a port
 import java.net.Socket;        // Incoming connections are represented as sockets
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.LinkedList;
 
 /**
  * Marian Zaki (COSC 2354)
@@ -14,7 +18,49 @@ public class EchoServer
 {
     /** The server will listen on this port for client connections */
     public static final int SERVER_PORT = 8754;
-
+    
+    /*disk hashtable that contains Integer and linked list of integers*/
+    private Hashtable<Integer,LinkedList<Integer>> disk;
+    
+    /*time stamp variable*/
+    Date timeStamp;
+    
+    /*availablitity check*/
+    protected boolean available;
+    
+    /*array list that contains all the servers*/
+    private ArrayList<EchoServer> allServers;
+    
+    /*array list that contains memory logs*/
+    private ArrayList<String[]> memoryLog;
+    
+    /*linked list of integers*/
+    private LinkedList<Integer> data;
+    
+    /**replicate method.
+     * push update to other server and commit to disk
+     */
+    private void replicate(){
+        
+    }
+    
+    /**check method.
+     * checks if each server has the latest update
+     * (i.e compares the linked list of each server)
+     * @return true if the linked list of each server is the same
+     */
+    private boolean check(){
+        return true;
+        
+    }
+    
+    /**updateServer method.
+     * updates EchoClient command to  server and use replicate() to update the other servers
+     */
+    public void updateServer(){
+        replicate();
+    }
+    
     /**
      * Main routine.  Just a dumb loop that keeps accepting new
      * client connections.
