eef1a16ea37cb80dbb2f35af99b774d8322d5083
sharonajayi <75191908+sharonajayi@users.noreply.github.com>
2021-05-06 00:21:07 -0500

    Updated files 2.0

--- Message.java
+++ Message.java
@@ -8,7 +8,7 @@ package server;
  *
  *  
  */
-public class Message implements java.io.Serializable
+public class Message 
 {
     /** The text string encoded in this Message object */
     public String theMessage;
@@ -23,6 +23,11 @@ public class Message implements java.io.Serializable
      * @param check
      *
      */
+    
+    public Message(String _msg){
+        theMessage = _msg;
+        
+    }
     public Message(String _msg, boolean check){
 	switch(_msg)
         {
@@ -32,8 +37,6 @@ public class Message implements java.io.Serializable
             case "R": theMessage = "Rollback"; break;
             
             case "V": theMessage = "Viewing data"; break;
-            
-            case "M": theMessage = "Memory Log being displayed"; break;
             
             default: 
                 theMessage = _msg;
